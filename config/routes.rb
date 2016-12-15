63245d173030f264dc3fcb4afdc43cae95906194
Guilherme Pinto Lima de Almeida <iugmali@gmail.com>
2016-12-15 19:03:40 +0000

    update readme

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#index'
-  get 'about', to: 'pages#about'
+  get 'sobre', to: 'pages#about'
 
 end
